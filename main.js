848be1f2dfea8632e9c1eddc4b33fa45ffd0d49d
Yur1kk <144416207+Yur1kk@users.noreply.github.com>
2023-10-20 11:29:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,84 @@
-const http = require('http');
-const fs = require('fs');
-const fastXmlParser = require('fast-xml-parser');
+const http = require('http'); // Підключаємо модуль 'http' для створення HTTP-сервера
+const fs = require('fs'); // Підключаємо модуль 'fs' для роботи з файловою системою
+const fastXmlParser = require('fast-xml-parser'); // Підключаємо модуль 'fast-xml-parser' для роботи з XML
 
+// Функція для розбору XML-даних
+function XMLParser(xmlString) {
+  // Налаштовуємо параметри для парсера XML
+  const options = {
+    attributeNamePrefix: '@', // Префікс для атрибутів
+    ignoreAttributes: false, // Враховувати атрибути як елементи
+    format: true, // Використовувати форматування
+  };
+
+  // Використовуємо бібліотеку 'fast-xml-parser' для парсингу рядка XML
+  return fastXmlParser.parse(xmlString, options);
+}
+
+// Функція для побудови XML-документу
+function XMLBuilder(data) {
+  // Налаштовуємо параметри для генератора XML
+  const builderOptions = {
+    attributeNamePrefix: '@', // Префікс для атрибутів
+    format: true, // Використовувати форматування
+  };
+
+  // Створюємо об'єкт генератора XML
+  const xmlBuilder = new fastXmlParser.j2xParser(builderOptions);
+
+  // Генеруємо XML-документ з вхідних даних
+  return xmlBuilder.parse(data);
+}
+
+// Створюємо HTTP-сервер
 const server = http.createServer((req, res) => {
+  // Перевіряємо, чи це GET-запит і чи URL - '/'
   if (req.method === 'GET' && req.url === '/') {
     // Читаємо дані з файлу data.xml
     const xmlData = fs.readFileSync('data.xml', 'utf8');
-    const options = {
-      ignoreAttributes: false, // Розглядає атрибути як елементи
-    };
 
-    // Аналізуємо XML-дані
-    const parsedData = fastXmlParser.parse(xmlData, options);
-    
-    // Фільтруємо дані, обираючи лише обрані категорії
-    const filteredData = parsedData.indicators.banksincexp.filter((item) => {
-      return item.txt === 'Доходи, усього' || item.txt === 'Витрати, усього';
-    });
+    // Розбираємо XML-дані за допомогою функції XMLParser
+    const parsedData = XMLParser(xmlData);
 
-    // Створюємо початок XML-відповіді з обгорненим <data> елементом
-    let xmlResponse = '<?xml version="1.0" encoding="UTF-8"?>\n<data>';
+    // Перевіряємо, чи вдалося розібрати дані та чи існує потрібна структура
+    if (parsedData && parsedData.indicators && parsedData.indicators.banksincexp) {
+      const banksincexpData = parsedData.indicators.banksincexp;
 
-    // Оброблюємо кожен обраний елемент та створюємо відповідну структуру XML в середині <data>
-    for (const item of filteredData) {
-      xmlResponse += '\n  <indicators>';
-      xmlResponse += `\n    <txt>${item.txt}</txt>`;
-      xmlResponse += `\n    <value>${item.value}</value>`;
-      xmlResponse += '\n  </indicators>';
+      // Фільтруємо дані, обираючи лише обрані категорії
+      const filteredData = banksincexpData.filter((item) => {
+        return item.txt === 'Доходи, усього' || item.txt === 'Витрати, усього';
+      });
+
+      // Створюємо початок XML-відповіді з обгорненим <data> елементом
+      let xmlResponse = '<?xml version="1.0" encoding="UTF-8"?>\n<data>';
+
+      // Оброблюємо кожен обраний елемент та створюємо відповідну структуру XML в середині <data>
+      for (const item of filteredData) {
+        xmlResponse += '\n  <indicators>';
+        xmlResponse += `\n    <txt>${item.txt}</txt>`;
+        xmlResponse += `\n    <value>${item.value}</value>`;
+        xmlResponse += '\n  </indicators>';
+      }
+
+      // Завершуємо XML-структуру, включаючи закриваючий </data> елемент
+      xmlResponse += '\n</data>';
+
+      // Відправляємо XML-відповідь зі статусом 200 (OK)
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.end(xmlResponse);
+    } else {
+      // Якщо дані некоректні, встановлюємо статус 500 (Internal Server Error) і надсилаємо відповідь "Invalid XML Data"
+      res.statusCode = 500;
+      res.end('Invalid XML Data');
     }
-
-    // Завершуємо XML-структуру, включаючи закриваючий </data> елемент
-    xmlResponse += '\n</data>';
-
-    // Відправляємо XML-відповідь зі статусом 200 (OK)
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(xmlResponse);
   } else {
+    // Якщо запит не підходить під умову, встановлюємо статус 400 (Bad Request) і надсилаємо відповідь "Bad Request"
     res.statusCode = 400;
     res.end('Bad Request');
   }
 });
 
-// Запускаємо сервер на порті 8000
+// Запускаємо сервер на порті 8000 і виводимо повідомлення про запуск в консоль
 server.listen(8000, () => {
   console.log('Сервер запущено');
 });
